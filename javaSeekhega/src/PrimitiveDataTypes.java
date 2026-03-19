66d70d2dd794a3baf24bbead3700a2849b209d52
Devrath Singh Rana <devrathsingh.rana@clearwateranalytics.com>
2026-03-20 00:18:06 +0530

    primitivedatatypes

---
+++ javaSeekhega/src/PrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args){
+        //integral
+        byte b = 127;//1 byte = 8 bits
+        short s = 32767;//2 bytes
+        int i = 2147483647;//4 bytes
+        long l = 9223372036854775807L;//8 bytes
+
+        //decimal
+        float f = 3.14f;//2 bytes
+        double d = 3.141592653589793;//8 bytes
+
+        //boolean
+        boolean bool = true;//1 bit
+
+        //character
+        char c = 'a';//2 bytes
+
+        //widening type conversion is implicit type conversion or automatic type coercion
+        int wideInt = b;
+        long wideLong = i;
+        double wideDouble = f;
+        System.out.println((char) 2);
+    }
+}
