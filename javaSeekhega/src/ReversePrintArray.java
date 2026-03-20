51461fd99a4acc5952748a9ae6ff88fbcecdf79a
Devrath Singh Rana <devrathsingh.rana@clearwateranalytics.com>
2026-03-20 23:14:44 +0530

    ReversePrintArray

---
+++ javaSeekhega/src/ReversePrintArray.java
@@ -0,0 +1,8 @@
+public class ReversePrintArray {
+    public static void main(String[] args){
+        int[] arr = new int[]{10,30,49,80};
+        for(int i=arr.length-1; i>=0; i--){
+            System.out.println(arr[i]);
+        }
+    }
+}
